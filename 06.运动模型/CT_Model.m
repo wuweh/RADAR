d4c55d336f1d7ff7db2758852f822e2ca3b8402b
hcheng1005 <139105918@qq.com>
2018-09-17 23:51:56 +0800

    update

--- 06.运动模型/CT_Model.m
+++ 06.运动模型/CT_Model.m
@@ -53,10 +53,11 @@ P21 =[  1 0 0 0 ;
  R1 = R;
   
   
- %初始化
+%初始化
 x(:,1) = [20,10,20,10,w]';
 Z1(:,1) = H*x(:,1)+sqrtm(R)*rand(2,1);
- %生成运动轨迹
+
+%生成运动轨迹
  for i = 2:SPM
     x(:,i) = F*x(:,i-1)+sqrtm(Q)*rand(5,1);
     Z1(:,i) = H*x(:,i)+sqrtm(R)*rand(2,1);
@@ -72,7 +73,7 @@ ux3 = x(1:4,1);
  end
  
 figure;
-subplot(121)
+subplot(221)
 hold on;
 plot(x(1,:),x(3,:),'-b.');
 hold on;
@@ -83,13 +84,26 @@ legend('真实值','测量值','CT预测值','CV预测值')
   
 D=sqrt((x(1,:)-Xkf(1,:)).^2+(x(3,:)-Xkf(3,:)).^2);%均方根误差
 D1=sqrt((x(1,:)-Xkf1(1,:)).^2+(x(3,:)-Xkf1(3,:)).^2);%均方根误差
-subplot(122)
+subplot(222)
 plot(D(1:SPM),'-bo','MarkerSize',4);
 hold on
 plot(D1(1:SPM),'-ro','MarkerSize',4);
 legend('CT模型误差','CV模型误差')
 
+x_p = 1:1:SPM;
+delta_x1=x(1,:)-Xkf(1,:);
+delta_y1=x(3,:)-Xkf(3,:);
+delta_x2=x(1,:)-Xkf1(1,:);
+delta_y2=x(3,:)-Xkf1(3,:);
 
- 
+subplot(223)
+plot(x_p,delta_x1,'-ro','MarkerSize',4);
+hold on
+plot(x_p,delta_x2,'-bo','MarkerSize',4);
+
+subplot(224)
+plot(x_p,delta_y1,'-ro','MarkerSize',4);
+hold on
+plot(x_p,delta_y2,'-bo','MarkerSize',4);
  
  
