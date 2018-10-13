09d5b1f680501b3f1f7ab92cd828bfe03bca103f
Hcheng1005 <139105918@qq.com>
2018-10-13 20:19:30 +0800

    update

--- 05.卡尔曼范例/05.kalman/kalman滤波/lmodeinitial.m
+++
@@ -1,9 +0,0 @@
-function [o,h,g,q,perr,xk]=lmodeinitial(T,r,zx,zy,vxks,vyks,perr2)
-o=[1,T,0,0;0,1,0,0;0,0,1,T;0,0,0,1];
-h=[1,0,0,0;0,0,1,0];
-g=[T^2/2,0;T,0;0,T^2/2;0,T];
-q=[10000,0;0,10000];
-xk=[zx(r-1);vxks(r-1);zy(r-1);vyks(r-1)];
-perr=perr2(1:4,1:4);
-    
-       
